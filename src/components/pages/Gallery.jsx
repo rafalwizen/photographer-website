c76a50e1c469f9df5fa9bb771d8b1c06c4195b1d
Rafal Wizen <rafal.wizen@gmail.com>
2024-09-05 19:17:29 +0200

    Disabling the ability to enlarge a photo - for testing purposes

--- src/components/pages/Gallery.jsx
+++ src/components/pages/Gallery.jsx
@@ -6,11 +6,11 @@ import './Gallery.css';
 
 const loadImages = async (galleryName, imagesSize) => {
     const indices = Array.from({ length: imagesSize }, (_, index) => index + 1);
-    const imagePromises = indices.map(index =>
-        fetch(`/images/${galleryName}/${index}-full.jpg`)
-            .then(response => response.ok ? `/images/${galleryName}/${index}-full.jpg` : null)
-            .catch(() => null)
-    );
+    // const imagePromises = indices.map(index =>
+    //     fetch(`/images/${galleryName}/${index}-full.jpg`)
+    //         .then(response => response.ok ? `/images/${galleryName}/${index}-full.jpg` : null)
+    //         .catch(() => null)
+    // );
 
     const thumbnailPromises = indices.map(index =>
         fetch(`/images/${galleryName}/${index}-small.jpg`)
@@ -18,59 +18,61 @@ const loadImages = async (galleryName, imagesSize) => {
             .catch(() => null)
     );
 
-    const images = await Promise.all(imagePromises);
+    // const images = await Promise.all(imagePromises);
     const thumbnails = await Promise.all(thumbnailPromises);
 
-    return { images: images.filter(img => img !== null), thumbnails: thumbnails.filter(img => img !== null) };
+    return thumbnails.filter(img => img !== null);
 };
 
 const Gallery = ({ galleryName }) => {
-    const [imageList, setImageList] = useState([]);
+    // const [imageList, setImageList] = useState([]);
     const [thumbnailList, setThumbnailList] = useState([]);
-    const [currentIndex, setCurrentIndex] = useState(null);
+    // const [currentIndex, setCurrentIndex] = useState(null);
 
     useEffect(() => {
         const loadImagesFromGallery = async () => {
             const gallery = galleryData.find(item => item.id === galleryName);
             const numberOfImages = gallery ? gallery.numberOfImages : null;
-            const { images, thumbnails } = await loadImages(galleryName, numberOfImages);
-            setImageList(images);
+            const thumbnails = await loadImages(galleryName, numberOfImages);
+            // setImageList(images);
             setThumbnailList(thumbnails);
         };
         loadImagesFromGallery();
     }, [galleryName]);
 
-    const openModal = (index) => {
-        setCurrentIndex(index);
-    };
+    // const openModal = (index) => {
+    //     setCurrentIndex(index);
+    // };
 
-    const closeModal = () => {
-        setCurrentIndex(null);
-    };
+    // const closeModal = () => {
+    //     setCurrentIndex(null);
+    // };
 
-    const showNextImage = () => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % imageList.length);
-    };
+    // const showNextImage = () => {
+    //     setCurrentIndex((prevIndex) => (prevIndex + 1) % imageList.length);
+    // };
 
-    const showPrevImage = () => {
-        setCurrentIndex((prevIndex) => (prevIndex - 1 + imageList.length) % imageList.length);
-    };
+    // const showPrevImage = () => {
+    //     setCurrentIndex((prevIndex) => (prevIndex - 1 + imageList.length) % imageList.length);
+    // };
 
     return (
         <div className="gallery-grid">
             {thumbnailList.map((thumbnail, index) => (
-                <div key={index} className="gallery-item" onClick={() => openModal(index)}>
+                <div key={index} className="gallery-item"
+                     // onClick={() => openModal(index)}
+                    >
                     <img src={thumbnail} alt={`Gallery image ${index + 1}`} />
                 </div>
             ))}
-            {currentIndex !== null && (
-                <ImageModal
-                    image={imageList[currentIndex]}
-                    onClose={closeModal}
-                    onNext={showNextImage}
-                    onPrev={showPrevImage}
-                />
-            )}
+            {/*{currentIndex !== null && (*/}
+            {/*    <ImageModal*/}
+            {/*        image={imageList[currentIndex]}*/}
+            {/*        onClose={closeModal}*/}
+            {/*        onNext={showNextImage}*/}
+            {/*        onPrev={showPrevImage}*/}
+            {/*    />*/}
+            {/*)}*/}
         </div>
     );
 };
