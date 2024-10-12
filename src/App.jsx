fda553c3be4579c3ad7ed15335c0419f036d386e
Rafal Wizen <rafal.wizen@gmail.com>
2024-10-12 21:41:04 +0200

    Add Google Analytics module

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from 'react';
 import Navbar from "./components/Navbar.jsx";
 import Sidebar from "./components/Sidebar.jsx";
 import Opinions from "./components/Opinions.jsx";
@@ -8,9 +9,20 @@ import GalleryContainer from "./components/pages/GalleryContainer.jsx";
 import Offer from "./components/pages/Offer.jsx";
 import Contact from "./components/pages/Contact.jsx";
 
-function App() {
+const TrackPageView = ({ logPageView }) => {
+    const location = useLocation();
+
+    useEffect(() => {
+        logPageView();
+    }, [location, logPageView]);
+
+    return null;
+};
+
+function App({ logPageView }) {
     return (
         <Router>
+            <TrackPageView logPageView={logPageView} />
             <div className="app-container">
                 <Navbar />
                 <div className="main-content">
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 import './i18n/i18n.jsx';
+import config from '../config/config.json'
+import ReactGA from 'react-ga4';
+
+const GA_MEASUREMENT_ID = config.google_analytics.measurement_id;
+
+ReactGA.initialize(GA_MEASUREMENT_ID);
+
+const logPageView = () => {
+    ReactGA.send('pageview', { page: window.location.pathname });
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <App logPageView={logPageView} />
     </React.StrictMode>
-)
+);
