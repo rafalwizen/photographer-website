1bf46c0a8e6eb9df0557427f034af25deb5c8604
Rafal Wizen <rafal.wizen@gmail.com>
2024-10-12 21:04:11 +0200

    Add links to social media

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import './About.css';
-import {useTranslation} from "react-i18next";
+import { useTranslation } from "react-i18next";
 
 const About = () => {
     const { t } = useTranslation();
 
     return (
-        <div className={'about'}>
+        <div className="about">
             <div className="content">
                 <p>{t('about.temp_p')}</p>
+                <div className="social-icons">
+                    <a href="https://www.instagram.com/p_rozbicki/" target="_blank" rel="noopener noreferrer">
+                        <img src="/public/images/Instagram_logo.png" alt="Facebook" />
+                    </a>
+                    <a href="https://www.rozbicki.mywed.com" target="_blank" rel="noopener noreferrer">
+                        <img src="/public/images/MyWed_logo.png" alt="Instagram" />
+                    </a>
+                </div>
             </div>
         </div>
     );
