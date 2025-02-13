faa958ed04406e278465e4671c82040e483ed3f2
Rafal Wizen <rafal.wizen@gmail.com>
2025-02-13 20:16:20 +0100

    Fix loading config.json and starting application

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './i18n/i18n.jsx';
-import config from '../config/config.json'
 import ReactGA from 'react-ga4';
 
-const GA_MEASUREMENT_ID = config.google_analytics.measurement_id;
+const configUrl = '/config/config.json';
 
-ReactGA.initialize(GA_MEASUREMENT_ID);
-
-const logPageView = () => {
-    ReactGA.send('pageview', { page: window.location.pathname });
+const loadConfig = async () => {
+    try {
+        const response = await fetch(configUrl);
+        if (!response.ok) {
+            throw new Error(`Failed to load config: ${response.statusText}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
 };
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App logPageView={logPageView} />
-    </React.StrictMode>
-);
+loadConfig().then((config) => {
+    if (config) {
+        const GA_MEASUREMENT_ID = config.google_analytics.measurement_id;
+        ReactGA.initialize(GA_MEASUREMENT_ID);
+
+        const logPageView = () => {
+            ReactGA.send('pageview', { page: window.location.pathname });
+        };
+
+        ReactDOM.createRoot(document.getElementById('root')).render(
+            <React.StrictMode>
+                <App logPageView={logPageView} />
+            </React.StrictMode>
+        );
+    } else {
+        console.error('Failed to load config, app may not work correctly.');
+    }
+});
