c0c650ec9e106d9737662a0b71423232e40cbe8b
Rafal Wizen <rafal.wizen@gmail.com>
2025-02-13 20:43:58 +0100

    Add simple gallery and new photos

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { useEffect } from 'react';
 import Navbar from "./components/Navbar.jsx";
-import Sidebar from "./components/Sidebar.jsx";
-import Opinions from "./components/Opinions.jsx";
 import About from "./components/pages/About.jsx";
-import GalleryContainer from "./components/pages/GalleryContainer.jsx";
-import Offer from "./components/pages/Offer.jsx";
 import Contact from "./components/pages/Contact.jsx";
+import GallerySimple from "./components/pages/GallerySimple.jsx";
 
 const TrackPageView = ({ logPageView }) => {
     const location = useLocation();
@@ -28,7 +25,8 @@ function App({ logPageView }) {
                 <div className="main-content">
                     <Routes>
                         <Route path={"/"} element={<About />} />
-                        <Route path={"/gallery"} element={<GalleryContainer />} />
+                        {/*<Route path={"/gallery"} element={<GalleryContainer />} />*/}
+                        <Route path={"/gallery"} element={<GallerySimple />} />
                         {/*<Route path={"/offer"} element={<Offer />} />*/}
                         <Route path={"/contact"} element={<Contact />} />
                     </Routes>
---
+++ src/components/pages/GallerySimple.jsx
@@ -0,0 +1,93 @@
+import { useState, useEffect } from "react";
+import "./GallerySimple.css";
+
+import img2 from "../../assets/images/1-3.jpg";
+import img3 from "../../assets/images/1-4.jpg";
+import img4 from "../../assets/images/1-4a.jpg";
+import img5 from "../../assets/images/1-6.jpg";
+import img6 from "../../assets/images/3-1.jpg";
+import img7 from "../../assets/images/3-4.png";
+import img8 from "../../assets/images/4-2.jpg";
+import img9 from "../../assets/images/4-3.jpg";
+import img10 from "../../assets/images/5-1.jpg";
+import img11 from "../../assets/images/6-1.jpg";
+import img12 from "../../assets/images/6-2.jpg";
+import img13 from "../../assets/images/6-4.jpg";
+import img14 from "../../assets/images/7-1.jpg";
+import img15 from "../../assets/images/7-2.jpg";
+import img16 from "../../assets/images/7-3.jpg";
+import img17 from "../../assets/images/7-4.jpg";
+import img18 from "../../assets/images/7-5.jpg";
+import img19 from "../../assets/images/8-1.jpg";
+import img20 from "../../assets/images/8-2.jpg";
+import img21 from "../../assets/images/9-1.jpg";
+import img22 from "../../assets/images/9-2.jpg";
+import img23 from "../../assets/images/9-4.jpg";
+import img24 from "../../assets/images/9-6.jpg";
+import img25 from "../../assets/images/9-8.jpg";
+import img26 from "../../assets/images/9-9.jpg";
+import img27 from "../../assets/images/9-10.jpg";
+import img28 from "../../assets/images/9-10a.jpg";
+import img29 from "../../assets/images/9-11.jpg";
+import img30 from "../../assets/images/9-13.jpg";
+import img31 from "../../assets/images/9-15.jpg";
+import img32 from "../../assets/images/10-1.jpg";
+import img33 from "../../assets/images/10-3.jpg";
+import img34 from "../../assets/images/10-4.jpg";
+import img35 from "../../assets/images/10-5.jpg";
+import img36 from "../../assets/images/10-5a.jpg";
+import img37 from "../../assets/images/10-6.jpg";
+import img38 from "../../assets/images/10-7.jpg";
+import img39 from "../../assets/images/10-10.jpg";
+
+const Gallery = () => {
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    useEffect(() => {
+        if (selectedImage) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "auto";
+        }
+    }, [selectedImage]);
+
+    const images = [
+        img2, img3, img4, img5, img6, img7, img8, img9, img10,
+        img11, img12, img13, img14, img15, img16, img17, img18, img19, img20,
+        img21, img22, img23, img24, img25, img26, img27, img28, img29, img30,
+        img31, img32, img33, img34, img35, img36, img37, img38, img39
+    ];
+
+    return (
+        <>
+            <div className="gallery-container">
+                {images.map((image, index) => (
+                    <div
+                        key={index}
+                        className="gallery-item"
+                        onClick={() => setSelectedImage(image)}
+                    >
+                        <img
+                            src={image}
+                            alt={`Cake ${index + 1}`}
+                            className="gallery-image"
+                            loading="lazy"
+                        />
+                    </div>
+                ))}
+            </div>
+
+            {selectedImage && (
+                <div className="modal-overlay" onClick={() => setSelectedImage(null)}>
+                    <div className="modal-content">
+                        <img src={selectedImage} alt="Selected cake" className="modal-image" />
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
+
+
+
+export default Gallery;
